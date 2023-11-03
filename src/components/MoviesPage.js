9ed8206c90663df87fe6a890db3f5f0bea4ed280
fay <wahomeimani@gmail.com>
2023-11-03 11:22:53 +0300

    complete

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { Route } from "react-router-dom";
+//import { Route } from "react-router-dom";
 import MoviesList from "./MoviesList";
+//import MoviesList from "./components/MovieList.js";
 
 function MoviesPage({ movies }) {
   return (
